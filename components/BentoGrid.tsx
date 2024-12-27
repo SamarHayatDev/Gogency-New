017e10401aa40de4572ced951979c4557d271bf9
Samar Hayat <samarhayat.dev@gmail.com>
2024-12-27 17:50:21 +0500

    update

--- components/BentoGrid.tsx
+++ components/BentoGrid.tsx
@@ -7,7 +7,7 @@ const BentoGrid = () => {
   return (
     <section className="container mx-auto max-w-screen-lg px-4 py-12">
       <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl lg:text-5xl mb-2">
-        Bento Grid
+        How it Works
       </h2>
       <p className="max-w-[800px] text-muted-foreground md:text-xl mt-4 my-2 mb-4">
         Explore our versatile inquiry dashboard: effortlessly track, manage, and
