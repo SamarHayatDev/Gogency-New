4987faca7b92f19ae509a8826280a49c4406249b
Samar Hayat <samarhayat.dev@gmail.com>
2024-12-31 13:11:17 +0500

    update

--- components/PricingTab.tsx
+++ components/PricingTab.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link"; // Import Link from Next.js
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 import {
   Card,
@@ -43,6 +44,10 @@ const plans = [
       "KPIs",
     ],
     cta: "Get Subscription",
+    monthlyLink:
+      "https://www.flygency.com/?plan=e7044c5b-447c-4580-af5d-7f05fd570455&priceId=price_1NnfTuG7sNvxSo7yDSr2raSk&price=99&duration=false",
+    annualLink:
+      "https://www.flygency.com/?plan=95fd404f-24d5-428c-95d9-e84332dc265e&priceId=%22price_1NnfTuG7sNvxSo7ycV90Zz2r&price=1069&duration=true",
   },
   {
     name: "Growth",
@@ -75,6 +80,10 @@ const plans = [
       "KPIs",
     ],
     cta: "Get Subscription",
+    monthlyLink:
+      "https://www.flygency.com/?plan=d78e69d5-dc53-42dc-8595-78ca6074d9a7&priceId=price_1NnfTuG7sNvxSo7yMnOiT8Yy&price=199&duration=false",
+    annualLink:
+      "https://www.flygency.com/?plan=95fd404f-24d5-428c-95d9-e84332dc265e&priceId=price_1NnfTuG7sNvxSo7y3MHmOgxn&price=2149&duration=true",
   },
   {
     name: "Business",
@@ -107,6 +116,10 @@ const plans = [
       "KPIs",
     ],
     cta: "Get Subscription",
+    monthlyLink:
+      "https://www.flygency.com/?plan=95fd404f-24d5-428c-95d9-e84332dc265e&priceId=price_1NnfTuG7sNvxSo7yVhHvvegL&price=499&duration=false",
+    annualLink:
+      "https://www.flygency.com/?plan=95fd404f-24d5-428c-95d9-e84332dc265e&priceId=price_1NnfTuG7sNvxSo7ycbGg6GWd&price=5390&duration=true",
   },
 ];
 
@@ -139,15 +152,16 @@ const PricingTab = () => {
                   <ul className="mt-2 space-y-1 text-sm">
                     {plan.featuresMonthly.map((feature, idx) => (
                       <li key={idx} className="flex items-center gap-2">
-                        <CircleCheck className="text-primary" />{" "}
-                        {/* Add CircleCheck icon */}
+                        <CircleCheck className="text-primary" />
                         {feature}
                       </li>
                     ))}
                   </ul>
                 </CardContent>
                 <CardFooter>
-                  <Button>{plan.cta}</Button>
+                  <Link target="_blank" href={plan.monthlyLink} passHref>
+                    <Button>{plan.cta}</Button>
+                  </Link>
                 </CardFooter>
               </Card>
             ))}
@@ -169,15 +183,16 @@ const PricingTab = () => {
                   <ul className="mt-2 space-y-1 text-sm">
                     {plan.featuresAnnual.map((feature, idx) => (
                       <li key={idx} className="flex items-center gap-2">
-                        <CircleCheck className="text-primary" />{" "}
-                        {/* Add CircleCheck icon */}
+                        <CircleCheck className="text-primary" />
                         {feature}
                       </li>
                     ))}
                   </ul>
                 </CardContent>
                 <CardFooter>
-                  <Button>{plan.cta}</Button>
+                  <Link target="_blank" href={plan.annualLink} passHref>
+                    <Button>{plan.cta}</Button>
+                  </Link>
                 </CardFooter>
               </Card>
             ))}
